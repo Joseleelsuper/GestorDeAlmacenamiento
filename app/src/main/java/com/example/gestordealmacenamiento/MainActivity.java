fd307b4e51d7e885841cdb7cbb329e8f8186264b
José Gallardo <JoseGallardoC@protonmail.com>
2024-02-16 15:44:53 +0100

    first commit

---
+++ app/src/main/java/com/example/gestordealmacenamiento/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.gestordealmacenamiento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Método para el botón de abrir las settings
+    public void openSettings(android.view.View view) {
+        android.content.Intent intent = new android.content.Intent(android.provider.Settings.ACTION_INTERNAL_STORAGE_SETTINGS);
+        startActivity(intent);
+    }
+}
